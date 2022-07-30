5f059649bbddc944f126f8e93415a4d5fa27b58e
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-07-31 01:41:14 +0530

    Automate scrape and deploy

--- routes.js
+++ routes.js
@@ -1,12 +1,12 @@
 import { products } from './data'
 import { Router } from '@layer0/core'
 
-const CORS= (res) => {
-    if (res){
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        res.setHeader('Access-Control-Allow-Methods', 'GET')
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    }
+const CORS = (res) => {
+  if (res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  }
 }
 
 export default new Router()
@@ -15,7 +15,7 @@ export default new Router()
     setResponseHeader('Access-Control-Allow-Origin', '*')
     setResponseHeader('Access-Control-Allow-Methods', 'GET')
     setResponseHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    serveStatic('images/:path*')
+    serveStatic(':path*')
   })
   .get('/products/all', ({ compute, cache }) => {
     cache({ edge: { maxAgeSeconds: 60 * 60 * 24 * 365 }, browser: false })
---
+++ scrape.js
@@ -0,0 +1,95 @@
+const fs = require('fs-extra')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const imageDomain = 'https://layer0-docs-layer0-ecommmerce-api-example-default.layer0-limelight.link/images'
+/*
+Format:
+    name: String
+    path: lower case string with slashes
+    description: Description in ul classes
+    prices: { price: { value: 80, currencyCode: 'USD' }, salePrice: null, retailPrice: null },
+    images: [ { url }, { url }, ... ]
+    slug: path with removed slashes
+    price: { value: 80, currencyCode: 'USD' },
+*/
+const products = {}
+
+const getProducts = async () => {
+  const { data } = await axios.request({
+    url: 'https://edgioswagshop.com/products',
+    method: 'get',
+    headers: {
+      Cookie:
+        'pcc_bpc=RLuvxSmozHSNx7c7ZMQVys+ee0dwmkvG2eqvEMA5bGn3GOnypNqaNHDxTiUFvd/lyFrUaHNn4D/bYILoDodtsqxaR0wAK5qNqT/aEcgYx9TOaAAgt5cIM2GAJuf9/e8jNORu5yxs8/LrOxNtp/R4l4lhUOohZYe22LrAeME9B9oEywZgN+NqHLa+Wyq77OqiMtX8k1c379gZVIAKqX8qOeqpxSUcUsOaO+/6VcLT3esESYI3egaewTDtXjXTJiN7l/QrZrFfRhW/jLfzgonHu6rCu0l7FN9XDWN3+kanaKgQpFOReyIGCNLpnkghuk7U8hgANt7RFjU+jrrsHPlRsqjGXNcbRgDKxSbAX/4o6787QFIzOzLPl5gYO4lq+l8rJhyGEUJQOX7wdequrBNnJRUHtYvgmc6X0BepgwXkmUmhXmrEfsN9Pqb8pEDbsTrwORWXQr5fV+nbwjZKzfnV1v2N+ArSjsJCdOAw3x1pJYkILyoAt5lofr+ZNh0C4B89PBCYdvy8LhpVe+ARsFTZdFqilOL0eGWy6WdHji1zYkF0CDKYD9E9EyeJaLBSdoRZ7ggOmP49rTtgbRUVh7mrBVMgPIoSYa3CSTLN0VvU0vM=;',
+    },
+  })
+  let $ = cheerio.load(data)
+  let pdpLinks = {}
+  $('.product-thumbnail').each((i, el) => {
+    let itemName = $(el).find('.thumbnail-description-container').find('h5').text()
+    pdpLinks[$(el).find('a').attr('href')] = { name: itemName }
+    let itemPrice = $(el).find('.thumbnail-description-container').find('p').text()
+    let slug = itemName
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .replace(/[^\w-]+/g, '')
+    let path = `/${slug}/`
+    products[itemName] = {
+      path,
+      slug,
+      name: itemName,
+      prices: {
+        price: { value: itemPrice.match(/^([a-z]*)([^\d])([\d.,]+)$/i)[3], currencyCode: itemPrice.match(/^([a-z]*)([^\d])([\d.,]+)$/i)[2] },
+        salePrice: null,
+        retailPrice: null,
+      },
+      price: {
+        value: itemPrice.match(/^([a-z]*)([^\d])([\d.,]+)$/i)[3],
+        currencyCode: itemPrice.match(/^([a-z]*)([^\d])([\d.,]+)$/i)[2],
+      },
+    }
+  })
+
+  for (const i of Object.keys(pdpLinks)) {
+    let url = i
+    if (url.includes('http')) {
+    } else {
+      url = `https://edgioswagshop.com${url}`
+    }
+    const { data } = await axios.request({
+      url,
+      method: 'get',
+      headers: {
+        Cookie:
+          'pcc_bpc=RLuvxSmozHSNx7c7ZMQVys+ee0dwmkvG2eqvEMA5bGn3GOnypNqaNHDxTiUFvd/lyFrUaHNn4D/bYILoDodtsqxaR0wAK5qNqT/aEcgYx9TOaAAgt5cIM2GAJuf9/e8jNORu5yxs8/LrOxNtp/R4l4lhUOohZYe22LrAeME9B9oEywZgN+NqHLa+Wyq77OqiMtX8k1c379gZVIAKqX8qOeqpxSUcUsOaO+/6VcLT3esESYI3egaewTDtXjXTJiN7l/QrZrFfRhW/jLfzgonHu6rCu0l7FN9XDWN3+kanaKgQpFOReyIGCNLpnkghuk7U8hgANt7RFjU+jrrsHPlRsqjGXNcbRgDKxSbAX/4o6787QFIzOzLPl5gYO4lq+l8rJhyGEUJQOX7wdequrBNnJRUHtYvgmc6X0BepgwXkmUmhXmrEfsN9Pqb8pEDbsTrwORWXQr5fV+nbwjZKzfnV1v2N+ArSjsJCdOAw3x1pJYkILyoAt5lofr+ZNh0C4B89PBCYdvy8LhpVe+ARsFTZdFqilOL0eGWy6WdHji1zYkF0CDKYD9E9EyeJaLBSdoRZ7ggOmP49rTtgbRUVh7mrBVMgPIoSYa3CSTLN0VvU0vM=;',
+      },
+    })
+    $ = cheerio.load(data)
+    products[pdpLinks[i]['name']]['description'] = $('.product-overview ul').parent().html()
+    products[pdpLinks[i]['name']]['images'] = []
+    $('[data-toggle="lightbox"]').each((_, el) => {
+      let imagePath = `/products/${pdpLinks[i]['name']}/${_}.png`
+      axios
+        .get($(el).attr('href'), {
+          responseType: 'arraybuffer',
+        })
+        .then((response) => Buffer.from(response.data, 'base64'))
+        .then((res) => {
+          fs.outputFile(`.${imagePath}`, res)
+            .then(() => {
+              console.log('✅')
+            })
+            .catch(console.log)
+        })
+      products[pdpLinks[i]['name']]['images'].push({ url: `${imageDomain}${imagePath}` })
+    })
+  }
+  try {
+    fs.outputFile('./data.js', `export const products= ${JSON.stringify(Object.keys(products).map((i) => products[i]))}`)
+    console.log('✅')
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+getProducts()
