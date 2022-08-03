b0b25e765e852b88eb09989ee09c726b83cec7d0
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-08-03 13:47:20 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -41,7 +41,7 @@ export default new Router()
       res.setHeader('Access-Control-Allow-Origin', '*')
       res.setHeader('Access-Control-Allow-Methods', 'GET')
       res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-      res.body = JSON.stringify(categories[req.params.slug])
+      res.body = JSON.stringify(Object.keys(categories).map(i => categories[i]).find(i => i.slug === req.params.slug))
       res.statusCode = 200
       res.statusMessage = 'OK'
     })
